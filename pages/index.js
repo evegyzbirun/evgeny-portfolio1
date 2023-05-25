0c39c650740ef9166cf8e9e925f08c097ee4daad
evegyzbirun <zbirunzbirun@gmail.com>
2023-05-25 14:43:25 -0700

    added image

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
-import { BsFillMoonStarsFill } from 'react-icons/bs'
-
+import { BsFillMoonStarsFill } from 'react-icons/bs';
+import { AiFillTwitterCircle, AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
+import Image from "next/image";
+import deved from "../public/evgeny1.jpg";
 
 
 export default function Home() {
@@ -16,16 +18,34 @@ export default function Home() {
       <main className="bg-white px-10">
         <section className=" bg-yellow-200 min-h-screen">
           <nav className="py-10 mb-14 flex justify-between">
-            <h1 className="text-xl">developed</h1>
+            <h1 className="text-xl font-burtons">developed</h1>
             <ul className="flex items-center">
               <li>
-                <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
               </li>
               <li>
                 <a className="bg-gradient-to-r from-cyan-600 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a>
               </li>
             </ul>
           </nav>
+
+          <div className="text-center p-12">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium">Evgeny Zbirun</h2>
+            <h3 className="text-2xl py-2">Developer</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">
+              About me.
+            </p>
+          </div>
+
+          <div className="text-5xl flex justify-center gap-16 py-4 text-gray-600">
+            <AiFillTwitterCircle />
+            <AiFillLinkedin />
+            <AiFillGithub />
+          </div>
+
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+            <Image src={deved}/>
+          </div>
         </section>
       </main>
     </div>
